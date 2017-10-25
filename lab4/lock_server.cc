1f4d4a482318863ec199ff857fefb467b11c8580
gousaiyang <gousaiyang@163.com>
2017-10-25 23:11:43 +0800

    lab4: Pass Part1

--- lab4/lock_server.cc
+++ lab4/lock_server.cc
@@ -8,27 +8,67 @@
 
 lock_server::lock_server()
 {
-    nacquire = 0;
+    VERIFY(pthread_mutex_init(&mutex, NULL) == 0);
+}
+
+lock_server::~lock_server()
+{
+    VERIFY(pthread_mutex_destroy(&mutex) == 0);
 }
 
 lock_protocol::status lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
 {
     lock_protocol::status ret = lock_protocol::OK;
+
     printf("stat request from clt %d\n", clt);
-    r = nacquire;
+
+    VERIFY(pthread_mutex_lock(&mutex) == 0);
+    r = lock_table.find(lid) == lock_table.end() ? -1 : lock_table[lid].nacquire;
+    VERIFY(pthread_mutex_unlock(&mutex) == 0);
+
     return ret;
 }
 
 lock_protocol::status lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &r)
 {
     lock_protocol::status ret = lock_protocol::OK;
+
     // Your lab4 code goes here
+    printf("acquire request from clt %d\n", clt);
+
+    VERIFY(pthread_mutex_lock(&mutex) == 0);
+    if (lock_table.find(lid) != lock_table.end()) {
+        while (lock_table[lid].flag) {
+            VERIFY(pthread_mutex_unlock(&mutex) == 0);
+            VERIFY(pthread_mutex_lock(&mutex) == 0);
+        }
+        lock_table[lid].flag = true;
+        lock_table[lid].holder = clt;
+        lock_table[lid].nacquire++;
+    } else {
+        lock_table[lid] = lock_state(true, clt, 1);
+    }
+    VERIFY(pthread_mutex_unlock(&mutex) == 0);
+    r = 0;
+
     return ret;
 }
 
 lock_protocol::status lock_server::release(int clt, lock_protocol::lockid_t lid, int &r)
 {
     lock_protocol::status ret = lock_protocol::OK;
+
     // Your lab4 code goes here
+    printf("release request from clt %d\n", clt);
+
+    VERIFY(pthread_mutex_lock(&mutex) == 0);
+    if (lock_table.find(lid) == lock_table.end() || !lock_table[lid].flag || lock_table[lid].holder != clt) {
+        r = -1;
+    } else {
+        lock_table[lid].flag = false;
+        r = 0;
+    }
+    VERIFY(pthread_mutex_unlock(&mutex) == 0);
+
     return ret;
 }
--- lab4/lock_server.h
+++ lab4/lock_server.h
@@ -5,17 +5,29 @@
 #define lock_server_h
 
 #include <string>
+#include <map>
+#include <pthread.h>
 #include "lock_protocol.h"
 #include "lock_client.h"
 #include "rpc.h"
 
+class lock_state {
+public:
+    bool flag;
+    int holder;
+    int nacquire;
+    lock_state() {}
+    lock_state(bool f, int h, int n): flag(f), holder(h), nacquire(n) {}
+};
+
 class lock_server {
 protected:
-    int nacquire;
+    std::map<lock_protocol::lockid_t, lock_state> lock_table;
+    pthread_mutex_t mutex;
 
 public:
     lock_server();
-    ~lock_server() {};
+    ~lock_server();
     lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
     lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
     lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);

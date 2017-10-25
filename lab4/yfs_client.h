bb33f8b6b11a781e177572bb84c9f705f9abb34f
gousaiyang <gousaiyang@163.com>
2017-10-25 18:11:15 +0800

    lab4: merge changes from lab3

--- lab4/yfs_client.h
+++ lab4/yfs_client.h
@@ -10,53 +10,65 @@
 #include "extent_client.h"
 #include <vector>
 
+#define MAX_FILENAME 255 // Maximum file name length allowed.
 
 class yfs_client {
-  extent_client *ec;
-  lock_client *lc;
- public:
+    extent_client *ec;
+    lock_client *lc;
 
-  typedef unsigned long long inum;
-  enum xxstatus { OK, RPCERR, NOENT, IOERR, EXIST };
-  typedef int status;
+public:
+    typedef unsigned long long inum;
+    enum xxstatus { OK, RPCERR, NOENT, IOERR, EXIST };
+    typedef int status;
 
-  struct fileinfo {
-    unsigned long long size;
-    unsigned long atime;
-    unsigned long mtime;
-    unsigned long ctime;
-  };
-  struct dirinfo {
-    unsigned long atime;
-    unsigned long mtime;
-    unsigned long ctime;
-  };
-  struct dirent {
-    std::string name;
-    yfs_client::inum inum;
-  };
+    struct fileinfo {
+        unsigned long long size;
+        unsigned long atime;
+        unsigned long mtime;
+        unsigned long ctime;
+    };
+    struct dirinfo {
+        unsigned long atime;
+        unsigned long mtime;
+        unsigned long ctime;
+    };
+    struct dirent {
+        std::string name;
+        yfs_client::inum inum;
+    };
 
- private:
-  static std::string filename(inum);
-  static inum n2i(std::string);
+private:
+    static bool filename_valid(std::string);
+    static bool inum_valid(inum);
+    int writedir(inum, std::list<dirent> &);
+    int createitem(inum, const char *, mode_t, inum &, uint32_t);
+    bool istype(inum, uint32_t);
 
- public:
-  yfs_client(std::string, std::string);
+public:
+    //yfs_client();
+    yfs_client(std::string, std::string);
+    ~yfs_client();
 
-  bool isfile(inum);
-  bool isdir(inum);
+    bool isfile(inum);
+    bool isdir(inum);
+    bool issymlink(inum);
 
-  int getfile(inum, fileinfo &);
-  int getdir(inum, dirinfo &);
+    int getfile(inum, fileinfo &);
+    int getdir(inum, dirinfo &);
+    int getsymlink(inum, fileinfo &);
 
-  int setattr(inum, size_t);
-  int lookup(inum, const char *, bool &, inum &);
-  int create(inum, const char *, mode_t, inum &);
-  int readdir(inum, std::list<dirent> &);
-  int write(inum, size_t, off_t, const char *, size_t &);
-  int read(inum, size_t, off_t, std::string &);
-  int unlink(inum,const char *);
-  int mkdir(inum , const char *, mode_t , inum &);
+    int setattr(inum, size_t);
+    int lookup(inum, const char *, bool &, inum &);
+    int create(inum, const char *, mode_t, inum &);
+    int readdir(inum, std::list<dirent> &);
+    int write(inum, size_t, off_t, const char *, size_t &);
+    int read(inum, size_t, off_t, std::string &);
+    int unlink(inum, const char *);
+    int mkdir(inum, const char *, mode_t, inum &);
+    int symlink(inum, const char *, const char *, inum &);
+    int readlink(inum, std::string &);
+
+    /** you may need to add symbolic link related methods here.*/
 };
 
-#endif 
+#endif

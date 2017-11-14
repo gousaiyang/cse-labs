b9ebc43df9941c4369507523eeb1101bdd269aca
gousaiyang <gousaiyang@163.com>
2017-11-14 23:49:37 +0800

    lab5: optimize memory usage

--- lab5/extent_server.cc
+++ lab5/extent_server.cc
@@ -12,41 +12,21 @@
 
 char vc_logfile[] = "extent_version.log";
 
-void extent_server::read_log_entries(std::vector<std::string> &log_entries)
-{
-    log_entries.clear();
-    std::ifstream fin(vc_logfile, std::ios_base::binary);
-    int cnt;
-    fin.read((char*)&cnt, sizeof(int));
-    if (!fin)
-        return;
-    char *buf = new char[DISK_SIZE];
-    for (int i = 0; i < cnt; ++i) {
-        fin.read(buf, DISK_SIZE);
-        if (!fin)
-            break;
-        log_entries.push_back(std::string(buf, DISK_SIZE));
-    }
-    delete[] buf;
-    fin.close();
-}
-
-void extent_server::write_log_entries(std::vector<std::string> &log_entries)
-{
-    std::ofstream fout(vc_logfile, std::ios_base::binary);
-    int cnt = log_entries.size();
-    fout.write((char*)&cnt, sizeof(int));
-    for (int i = 0; i < cnt; ++i)
-        fout.write(log_entries[i].c_str(), DISK_SIZE);
-    fout.close();
-}
-
 extent_server::extent_server()
 {
     im = new inode_manager();
 
-    std::ofstream fout(vc_logfile, std::ios_base::app);
-    fout.close();
+    std::fstream f(vc_logfile, std::ios_base::in | std::ios_base::out | std::ios_base::binary);
+    if (!f.is_open()) {
+        printf("Error: cannot open version log file\n");
+        exit(1);
+    }
+
+    int cnt = 0;
+    f.read((char*)&cnt, sizeof(int));
+    if (f.eof())
+        f.write((char*)&cnt, sizeof(int));
+    f.close();
 
     int unused;
     commit(0, unused);
@@ -128,11 +108,15 @@ int extent_server::commit(uint32_t, int &)
 {
     printf("extent_server: commit\n");
 
-    std::vector<std::string> log_entries;
-    read_log_entries(log_entries);
-    log_entries.resize(++im->current_version);
-    log_entries.push_back(std::string(im->get_disk_ptr(), DISK_SIZE));
-    write_log_entries(log_entries);
+    std::fstream f(vc_logfile, std::ios_base::in | std::ios_base::out | std::ios_base::binary);
+    int cnt;
+    f.read((char*)&cnt, sizeof(int));
+    f.seekp(++im->current_version * DISK_SIZE, std::ios_base::cur);
+    f.write(im->get_disk_ptr(), DISK_SIZE);
+    f.seekp(0);
+    cnt = im->current_version + 1;
+    f.write((char*)&cnt, sizeof(int));
+    f.close();
     im->uncommitted = false;
 
     return extent_protocol::OK;
@@ -142,16 +126,20 @@ int extent_server::undo(uint32_t, int &)
 {
     printf("extent_server: undo\n");
 
-    std::vector<std::string> log_entries;
-    read_log_entries(log_entries);
+    std::ifstream fin(vc_logfile, std::ios_base::binary);
 
     if (im->uncommitted) {
-        memcpy(im->get_disk_ptr(), log_entries[im->current_version].c_str(), DISK_SIZE);
+        fin.seekg(sizeof(int) + im->current_version * DISK_SIZE);
+        fin.read(im->get_disk_ptr(), DISK_SIZE);
         im->uncommitted = false;
     } else {
-        if (im->current_version >= 1)
-            memcpy(im->get_disk_ptr(), log_entries[--im->current_version].c_str(), DISK_SIZE);
+        if (im->current_version >= 1) {
+            fin.seekg(sizeof(int) + --im->current_version * DISK_SIZE);
+            fin.read(im->get_disk_ptr(), DISK_SIZE);
+        }
     }
+
+    fin.close();
 
     return extent_protocol::OK;
 }
@@ -160,11 +148,17 @@ int extent_server::redo(uint32_t, int &)
 {
     printf("extent_server: redo\n");
 
-    std::vector<std::string> log_entries;
-    read_log_entries(log_entries);
+    std::ifstream fin(vc_logfile, std::ios_base::binary);
+    int cnt;
+    fin.read((char*)&cnt, sizeof(int));
 
-    if (im->current_version + 1 < log_entries.size())
-        memcpy(im->get_disk_ptr(), log_entries[++im->current_version].c_str(), DISK_SIZE);
+    if (im->current_version + 1 < cnt) {
+        fin.seekg(++im->current_version * DISK_SIZE, std::ios_base::cur);
+        fin.read(im->get_disk_ptr(), DISK_SIZE);
+        im->uncommitted = false;
+    }
+
+    fin.close();
 
     return extent_protocol::OK;
 }
